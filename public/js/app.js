7fcb76ea62ef54ab7d427fa5e10f4138db3a4bbc
Greg Mojonnier <gregmojonnier@gmail.com>
2016-07-17 10:09:21 -0700

    Started migrating chat page to use angular.

--- public/js/app.js
+++ public/js/app.js
@@ -4,5 +4,8 @@ app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: '/templates/register.html',
         controller: 'RegisterCtrl'
+    }).when('/chat', {
+        templateUrl: '/templates/chat.html',
+        controller: 'ChatCtrl'
     }).otherwise({redirectTo: '/'});
 }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,14 +1,17 @@
 var controllers = angular.module('chatAppControllers', []);
-controllers.controller('RegisterCtrl', function($scope, $http) {
+controllers.controller('RegisterCtrl', function($scope, $http, $location) {
     $scope.createUser = function() {
         $http.post('/user', {'name': $scope.username})
             .then(function success(response) {
                 $.cookie('_chatUser', response.data.name);
                 $.cookie('_chatId', response.data.id);
-                window.location.replace('#/chat');
+                $location.url('/chat');
             }, function error(response) {
                 $scope.registrationError = response.data.error;
                 $scope.username = '';
             });
     };
+});
+
+controllers.controller('ChatCtrl', function($scope, $http, $location) {
 });
